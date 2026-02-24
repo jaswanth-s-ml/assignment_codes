93bafe8538474d869cce628748e2774fe47282b6
jaswanth-s-ml <jashus3004@gmail.com>
2026-02-25 00:16:48 +0530

    Update m1_q1.java

--- m1_q1.java
+++ m1_q1.java
@@ -1,2 +1,17 @@
+import java.util.*;
 public class m1_q1 {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      if(a>0)
+      {
+          System.out.println("positive");
+      } else if(a<0){
+          System.out.println("negative");
+      }else {
+          System.out.println("zero");
+      }
+    }
+}
+
 }
